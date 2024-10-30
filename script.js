17832d261e505dccf3cbf5759fb17447b967d4a4
AndelKaz <149527003+AndelKaz@users.noreply.github.com>
2024-10-30 19:09:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const myAge = "13";
+console.log(myAge);
+
+const myName = "Andrew";
+console.log(myName);
+
+let isStudent = true;
+console.log(isStudent);
+
+const myString = "Be yourself";
+console.log(myString);
+
+
+let myNumber = 5;
+myNumber +=10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const userName = prompt("Type your name:");
+alert("Hi, " + userName + "!");
+
+const isConfirmed = confirm("You sure?");
+if (isConfirmed) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено");
+}
+
+alert("Це може бути небезпечно.");
+let proceed = confirm("Ви бажаєте продовжити?");
+if (proceed) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено");
+}
+
